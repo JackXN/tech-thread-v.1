5b532a7713b969ae96b5fd1559d6a7d05f095c28
Jack Rigan <riganjack40@gmail.com>
2021-09-03 22:30:47 -0600

    deleted craco.config and tailwind.config

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import "../Styles/Header.css"
+
+
 
 function Header() {
     return (
-        <div>
-            
+        <div className='header'>
+            <h1>Hello</h1>
+            {/* Header Left */}
+            <div className='header_left'>
+<img src='https://wpguru.co.uk/wp-content/uploads/2019/08/124010.png' alt='facebook-logo'/>
+
+            </div>
+            {/* Header middle */}
+            <div className='header_middle'></div>
+             {/* Header right */}
+             <div className='header_right'></div>
         </div>
     )
 }
