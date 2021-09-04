f4541b7b09c07d13aecfee260068cc006e11f1ea
Jack Rigan <riganjack40@gmail.com>
2021-09-04 17:48:48 -0600

    updated input form styling

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,19 +1,16 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import { Avatar } from '@material-ui/core';
 import "../Styles/InputBox.css";
-
-
-
+import VideocamIcon from '@material-ui/icons/Videocam';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
 function InputBox() {
-
-
 
 
 const handleSubmit = (e) => {
    e.preventDefault();
    console.log('submitted')
-   
 }
 
 
@@ -24,16 +21,28 @@ const handleSubmit = (e) => {
             <div className='input_top'>
 <Avatar/>       
 
-<form onSubmit={handleSubmit}>
+<form onSubmit={handleSubmit} className='inputBox_input'>
     <input type='text' placeholder={'Whats on your mind?'}/>
-<input placeholder='imae URL (optional)'/>
+<input placeholder='Image URL (optional)'/>
 <button type='submit' onSubmit={handleSubmit} hidden>this button is hiding</button>
 </form>
             </div>
 
             {/* Input Bottom */}
             <div className='input_bottom'>
-                
+                <div className='inputBox_option'>
+                    <VideocamIcon style={{color:"red"}}/>
+                    <h3>Live Video</h3>
+                </div>
+
+                <div className='inputBox_option'>
+                    <CameraAltIcon style={{color:"green"}}/>
+                    <h3>Photo/Video</h3>
+                </div>
+                <div className='inputBox_option'>
+                    <EmojiEmotionsIcon style={{color:"orange"}}/>
+                    <h3>Feeling/Activity</h3>
+                </div>
             <div className='input_option'>
 
             </div>
--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -4,10 +4,18 @@ import Story from './Story';
 function StoryReel() {
     return (
         <div className='storyReel'>
-            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg' profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e' title='Don Julio'/>
-            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg' profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e' title='Don Julio'/>
-            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg' profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e' title='Don Julio'/>
-            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg' profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e' title='Don Julio'/>
+            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg' 
+            profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e'
+             title='Don Julio'/>
+            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg'
+             profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e' 
+             title='Don Julio'/>
+            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg' 
+            profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e' 
+            title='Don Julio'/>
+            <Story image='https://www.giantbomb.com/a/uploads/original/2/22072/1032678-wowa22_7x8jxr41to.jpg' 
+            profileSrc='https://qph.fs.quoracdn.net/main-qimg-2fa4964e0f271e98878f2444fd90238e' 
+            title='Don Julio'/>
         </div>
     )
 }
