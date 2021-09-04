454b83f76b5cf61a8917ff0dd7db8c1cf1a05a82
Jack Rigan <riganjack40@gmail.com>
2021-09-04 01:58:41 -0600

    Created handleSubmit for form submission

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,22 +1,50 @@
 import React from 'react'
 import { Avatar } from '@material-ui/core';
 import "../Styles/InputBox.css";
+
+
+
+
 function InputBox() {
+
+
+
+
+const handleSubmit = (e) => {
+   e.preventDefault();
+   console.log('submitted')
+   
+}
+
+
+
     return (
         <div className='inputBox'>
             {/* Input Top */}
             <div className='input_top'>
 <Avatar/>       
 
-<form>
+<form onSubmit={handleSubmit}>
     <input type='text' placeholder={'Whats on your mind?'}/>
 <input placeholder='imae URL (optional)'/>
+<button type='submit' onSubmit={handleSubmit} hidden>this button is hiding</button>
 </form>
             </div>
 
             {/* Input Bottom */}
             <div className='input_bottom'>
+                
+            <div className='input_option'>
 
+            </div>
+
+            <div className='input_option'>
+                
+                </div>
+                
+                <div className='input_option'>
+                
+                </div>
 
             </div>
 
