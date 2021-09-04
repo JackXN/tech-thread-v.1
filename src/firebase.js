07c1f1b499f43ebff10dea1165bc939e238e8243
Jack Rigan <riganjack40@gmail.com>
2021-09-03 19:37:56 -0600

    populated firebase.js with config vars

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAOKyOmkmJMeSkOYwEDpXVJ8gCwAHwzAs0",
+    authDomain: "techthread-v1.firebaseapp.com",
+    projectId: "techthread-v1",
+    storageBucket: "techthread-v1.appspot.com",
+    messagingSenderId: "779110798696",
+    appId: "1:779110798696:web:3deecf9e2a9c77910832dd",
+    measurementId: "G-4KYGRY82BG"
+  };
+
+  
